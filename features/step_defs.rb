573ca928eabb1eb5e8382c0ae75cf1ee4e367e2e
Peter Kappus <peterk@peterkappus.com>
2019-03-09 11:27:12 +0000

    Added simplified step_defs and removed old ones.

--- features/step_defs.rb
+++ features/step_defs.rb
@@ -3,3 +3,7 @@ When("I search for {string}") do |string|
   fill_in "q", with: string
   click_on "Google Search", match: :first
 end
+
+Then("I should see {string}") do |string|
+  page.should have_content(string)
+end
